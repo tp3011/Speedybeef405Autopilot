010e09d45324e1fa329b054f29c0c25a3ad41b50
tp3011 <143531790+tp3011@users.noreply.github.com>
2024-12-29 19:36:41 -0500

    Update MainMission.py
    added landing, payload drop finished

--- MainMission.py
+++ MainMission.py
@@ -10,6 +10,9 @@ from main import vehicle
 #set waypoint, change mode, payload operation, takeoff, land, kill
 
 def takeoff(v): #function that sets up take off for a given pitch angle
+    vehicle.parameters["TKOFF_THR_MINACC"] = 0
+    vehicle.parameters["ARSPD_USE"] = 0
+    
     if not v.mode == VehicleMode("AUTO"):
         v.mode =VehicleMode("AUTO")
         while v.mode != VehicleMode("AUTO"):
@@ -34,6 +37,7 @@ def payload_drop(v):
     target_lat = float(input("Please provide target latitude :\n"))
     target_long = float(input("Target longitude:\n"))
     target_alt = float(input("Height above target altitude (for payload DROP):\n"))
+    target_pos = [target_lat, target_long,target_alt]
     drop_list=[]
 
     drop_waypoint = Command(0,0,0,
@@ -65,11 +69,57 @@ def payload_drop(v):
 def payload_delivery(v):
     pass
 
-
+def landing(v, delivery=False, land_position, tkoff_heading =0):
+    
+    if not delivery:
+        print("Returning home and landing")
+        init_land_command = Command(0,0,0,
+                               mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                               mavutil.mavlink.MAV_CMD_DO_LAND_START,
+                               0,1,
+                               0,0,0,0,0,0,0)
+        #Will have to adjust lat/long to match better, but general idea
+        approach_waypoint = Command(0,0,0,
+                                    mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                                    mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+                                    0,1,
+                                    0,5,0,tkoff_heading,
+                                    32.59204,-97.48064, 25)
+        
+        land_command = Command(0,0,0,
+                               mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                               mavutil.mavlink.MAV_CMD_NAV_LAND,
+                               0,0,0,tkoff_heading,
+                               land_position[0],land_position[1],0)
+        
+    else:
+        init_land_command = Command(0,0,0,
+                               mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                               mavutil.mavlink.MAV_CMD_DO_LAND_START,
+                               0,1,
+                               0,0,0,0,0,0,0)
+        #Will have to adjust lat/long to match better, but general idea
+        approach_waypoint = Command(0,0,0,
+                                    mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                                    mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
+                                    0,1,
+                                    0,5,0,0,
+                                    32.59204,-97.48064, 25)
+        #target alt must match 0 relative to DLZ
+        land_command = Command(0,0,0,
+                               mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
+                               mavutil.mavlink.MAV_CMD_NAV_LAND,
+                               0,1,0,0,
+                               land_position[0],land_position[1],land_position[2])
+        
+                                    
+        
 if __name__ == "__main__":
     cmds = vehicle.commands
     cmds.clear()
 
+    #getting vehicle initial position (imsure theres a better way)
+    home_position = [vehicle.location.global_frame.lat,vehicle.location.global_frame.lon]
     #inserting Takeoff command in queue
     cmds.add(takeoff(vehicle))
 
