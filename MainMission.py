7b98549e5bebf4823789b034900a1aa72105358d
tp3011 <143531790+tp3011@users.noreply.github.com>
2025-01-16 12:36:34 -0500

    Update MainMission.py

--- MainMission.py
+++ MainMission.py
@@ -9,6 +9,12 @@ from main import check_GPS_status, arm_vehicle, connection_string, baud_rate
 
 #set waypoint, change mode, payload operation, takeoff, land, kill
 
+######################### PROGRAM HAS TO BE UPDATED TO WORK WITH MAVUTIL 2.0 ####################################
+#################################### CURRENTLY NOT UPLOADING MISSION ##################################
+
+
+
+
 def takeoff(v): #function that sets up take off for a given pitch angle
     vehicle.parameters["TKOFF_THR_MINACC"] = 0
     vehicle.parameters["ARSPD_USE"] = 0
