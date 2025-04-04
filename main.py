edbe2e437175b80c80ff5864f07a3ca29eeecef0
tp3011 <143531790+tp3011@users.noreply.github.com>
2025-04-04 17:03:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import time
-import json
-from dronekit import connect, VehicleMode, Command
+from dronekit import connect, VehicleMode
 from pymavlink import mavutil
-import msvcrt
 import threading
 import sys
 import keyboard
 
-connection_string = "COM8" #"tcp:127.0.0.1:5762"
-baud_rate = 9600
+connection_string = "COM7"#"udp:127.0.0.1:14551"
+baud_rate = 57600
 print("This program works by iteration for now. Follow the instructions to complete the arming of the vehicle.\n"
       "Press 'c' at any time to shutdown.")
 time.sleep(1)
@@ -23,12 +21,29 @@ def check_GPS_status(vehicle):
 
 def check_safety_param(vehicle):
     vehicle.parameters["ARMING_CHECK"]=0
+    vehicle.parameters["RTL_ALT"] = int(0)
+    vehicle.parameters["ARMING_REQUIRE"] = 0
+    vehicle.parameters["WP_RETURN_AFTER"] = 0
+    vehicle.parameters["ARSPD_USE"] = 0
+    vehicle.parameters['TKOFF_THR_MINACC'] = 0
+    vehicle.parameters['TKOFF_THR_DELAY'] = 0
+    vehicle.parameters['THR_MAX'] = 100
+    vehicle.parameters['TKOFF_THR_MAX'] = 100
+    vehicle.parameters["TKOFF_LVL_ALT"] = 10
+    vehicle.parameters["TKOFF_LVL_PITCH"] = 7
+    vehicle.parameters["PTCH_LIM_MAX_DEG"] = 15
+    vehicle.parameters["PTCH_LIM_MIN_DEG"] = -10
+    vehicle.parameters["ROLL_LIMIT_DEG"] = 15
+    vehicle.parameters["RTL_AUTOLAND"] = 2
+    time.sleep(3)
+
     while vehicle.parameters["ARMING_CHECK"] != 0:
         print("Waiting for arming checks to disable... Press 'c' if UNINTENDED")
         time.sleep(5)
     else:
         print("Arming check disabled... Checking GPS status")
         while vehicle.gps_0.fix_type != 2:
+            break
             time.sleep(2)
         else: print("GPS signal acquired. Ready to arm vehicle...")
 
@@ -42,22 +57,24 @@ def check_control_surfaces(vehicle):
     print("Testing pitch up/down")
     vehicle.channels.overrides['2'] = vehicle.parameters['SERVO2_MAX']
     time.sleep(1)
-    vehicle.channels.overrides['2'] = vehicle.parameters['SERVO2_MIN']
+    vehicle.channels.overrides['2'] = vehicle.parameters['SERVO2_min']
     time.sleep(1)
     vehicle.channels.overrides['2'] = vehicle.parameters['SERVO2_TRIM']
     print("Testing rudder")
     vehicle.channels.overrides['4'] = vehicle.parameters['SERVO4_MAX']
     time.sleep(1)
-    vehicle.channels.overrides['4'] = vehicle.parameters['SERVO4_MIN']
+    vehicle.channels.overrides['4'] = vehicle.parameters['SERVO4_min']
     time.sleep(1)
-    vehicle.channels.overrides['4'] = vehicle.parameters['SERVO4_TRIM']
     print("Control surface tests done")
 
 
 
 
 def arm_vehicle(vehicle):
-    vehicle.mode = VehicleMode("AUTO")
+
+    vehicle.mode = VehicleMode("MANUAL")
+    while vehicle.mode != VehicleMode("MANUAL"):
+        time.sleep(.5)
     vehicle.armed = True
     while not vehicle.armed:
         print("Waiting to arm")
@@ -77,36 +94,39 @@ def listen_for_shutdown():
     sys.exit(0)
 
 
+if __name__ == "__main__":
+    print("This program works by iteration for now. Follow the instructions to complete the arming of the vehicle.\n"
+          "Press 'c' at any time to shutdown.")
 
-listener_thread = threading.Thread (target=listen_for_shutdown, daemon=True)
-listener_thread.start()
-shutdown_event = threading.Event()
-print("press 'f' to connect to vehicle")
-while not keyboard.is_pressed('f'):
-    time.sleep(0.05)
+    listener_thread = threading.Thread (target=listen_for_shutdown, daemon=True)
+    listener_thread.start()
+    shutdown_event = threading.Event()
+    print("press 'f' to connect to vehicle")
+    while not keyboard.is_pressed('f'):
+        time.sleep(0.05)
 
-#code to connect to plane
+    #code to connect to plane
 
-print("Confirmation received! Starting connection...")
+    print("Confirmation received! Starting connection...")
 
-vehicle = connect(ip=connection_string, baud = baud_rate, wait_ready=True)
-print("Connection successful")
+    vehicle = connect(ip=connection_string, baud = baud_rate, wait_ready=True)
+    print("Connection successful")
 
-print("press 's' to disable safety checks... FOR TESTING ONLY. Press 'c' to exit test")
-keyboard.add_hotkey("s", lambda: check_safety_param(vehicle))
-keyboard.wait()
+    print("press 's' to disable safety checks... FOR TESTING ONLY. Press 'c' to exit test")
+    keyboard.add_hotkey("s", lambda: check_safety_param(vehicle))
+    keyboard.wait()
 
-print("Press 't' to test control surfaces. Press 'p' to skip testing")
-while True:
-    if keyboard.is_pressed('t'):
-        check_control_surfaces(vehicle)
-        break
-    elif keyboard.is_pressed('p'):
-        print("Skipping testing")
-        break
+    print("Press 't' to test control surfaces. Press 'p' to skip testing")
+    while True:
+        if keyboard.is_pressed('t'):
+            check_control_surfaces(vehicle)
+            break
+        elif keyboard.is_pressed('p'):
+            print("Skipping testing")
+            break
 
-print("press 'a' to arm vehicle... MAKE SURE TO COMPLY WITH SAFETY REGULATIONS")
-keyboard.add_hotkey("a", lambda : arm_vehicle(vehicle))
+    print("press 'a' to arm vehicle... MAKE SURE TO COMPLY WITH SAFETY REGULATIONS")
+    keyboard.add_hotkey("a", lambda : arm_vehicle(vehicle))
 
 
 
